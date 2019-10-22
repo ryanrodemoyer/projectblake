74ca38c7f7790c67752b8dbfead9de8113d16536
Ryan Rodemoyer <ryan.rodemoyer@mortgagecadence.com>
2019-10-22 13:55:51 -0600

    auth callback controller

---
+++ ProjectBlake/Startup1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ProjectBlake.Startup1))]
+
+namespace ProjectBlake
+{
+    public class Startup1
+    {
+        private const string DS_INTEGRATION_KEY = "5b79f171-12d7-4d81-87a9-da99cd122ff7";
+
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
